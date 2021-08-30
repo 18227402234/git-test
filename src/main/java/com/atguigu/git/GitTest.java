befb5800b8482655430340f540dd47840092364f
zxx <3071410791@qq.com>
2021-08-30 22:15:17 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zxx
+ * @create 2021-08-30 22:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello , git!");
+    }
+}
